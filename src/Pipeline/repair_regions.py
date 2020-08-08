8975b65a023a2551e59ee53071dce35c28206d53
Felipe Ippolito <felipeippolito@gmail.com>
2020-08-07 23:54:28 -0300

    Limit region size and save dataframe to file

--- src/Pipeline/repair_regions.py
+++ src/Pipeline/repair_regions.py
@@ -21,12 +21,12 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def interpolation(Series, error, max_size=5, **kwargs):
+def interpolation(Series, error, max_interpolation_size=5, **kwargs):
   error_reg = list_2_regions(error)
   reg_size = [i[1] - i[0] for i in error_reg]
 
   interpol_reg = [error_reg[i]
-                  for i in range(len(reg_size)) if reg_size[i] <= max_size]
+                  for i in range(len(reg_size)) if reg_size[i] <= max_interpolation_size]
   error_interpol = regions_2_list(interpol_reg, len(Series))
 
   error.loc[error_interpol] = False
@@ -34,44 +34,36 @@ def interpolation(Series, error, max_size=5, **kwargs):
   Series.fillna(-100000)
   Series.loc[error_interpol] = np.nan
 
-  Series = Series.interpolate(method='linear', limit=max_size)
+  Series = Series.interpolate(method='linear', limit=max_interpolation_size)
   Series.loc[Series == -100000] = np.nan
 
-  return Series, error.tolist()
+  return Series, error.tolist(), error_interpol
 
 
-def predict_region(xgb, prediction, df_recurrent, label, dcols, p, lookbackSize):
+def predict_region(xgb, df_recurrent, label, dcols, p, lookbackSize):
     df_recurrent = df_recurrent.copy(deep=True)
     start, stop = p[0], p[1]
     y_predict = []
     for j in range(start, stop):
       pred_ = xgb.predict(df_recurrent.loc[[j]])
-      #prediction.loc[j,label] = pred_
       y_predict.append(pred_)
       matrix = df_recurrent.loc[j+1:j+1+lookbackSize, dcols].values
       np.fill_diagonal(matrix, pred_)
       df_recurrent.loc[j+1:j+1+lookbackSize, dcols] = matrix
-    #return prediction, start, stop
     return y_predict
 
 
-def repair_regions(df, label, max_size=None, lookbackSize=None, extra_features=None):
-  """
+def repair_regions(df, label, max_region_size=None, lookbackSize=None, extra_features=None, **kwargs):
+  '''
   df
   label
-  max_size
+  max_interpolation_size
   lookbackSize
   extra_features - list
-  """
-
-  df[['Date', 'Time']] = df['Data / Hora'].str.split(expand=True)
-  df[['Hora', 'Min', 'Seg']] = df['Time'].str.split(':', expand=True)
-  df['Hora'] = df['Hora'].astype(int)
-  df['Min'] = df['Min'].astype(int)
-  df['Seg'] = df['Seg'].astype(int)
+  '''
 
   attribute = label.split('_')[0]
-  cols_attribute = [i for i in df.columns if attribute in i]
+  cols_attribute = [i for i in df.columns if attribute in i and 'interpol' not in i]
   df_att = df[extra_features + cols_attribute].copy(deep=True)
 
   # Create Delay | LookBack
@@ -106,32 +98,30 @@ def repair_regions(df, label, max_size=None, lookbackSize=None, extra_features=N
       [label] + [label+'_error']
 
   df_recurrent = df_att.drop(columns=drop_cols).copy(deep=True)
-  prediction = df_att[[label, label + '_error']].copy(deep=True)
+  df[label + '_pred'] = df[label]
 
   predict_regions = list_2_regions(df_att[label + '_error'])
 
-  logging.info(f'Number of regions: {len(predict_regions)}')
-
   dcols = ['delay_' + str(i+1) for i in range(lookbackSize)]
-  # time_start = time.time()
 
-  # for p in predict_regions[0:30]:
-  #   for i in range(p[0], p[1]):
-  #     pred_ = xgb.predict(df_recurrent.loc[[i]])
-  #     prediction.loc[i, label] = pred_
-  #     matrix = df_recurrent.loc[i + 1 : i + lookbackSize + 1, dcols].values
-  #     np.fill_diagonal(matrix, pred_)
-  #     df_recurrent.loc[i + 1 : i + lookbackSize + 1, dcols] = matrix
+  # Limit Regions size by max_region_size
+  nr = len(predict_regions)
+  reg_size = [i[1] - i[0] for i in predict_regions]
+  predict_regions = [predict_regions[i]
+                     for i in range(len(reg_size)) if reg_size[i] < max_region_size]
+  logging.info(f'Number of regions: {nr} -> {len(predict_regions)}')
+
+  df[label + '_repaired'] = regions_2_list(predict_regions, len(df))
 
   with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-    futures = {executor.submit(predict_region, xgb, prediction, df_recurrent,
-                               label, dcols, p, lookbackSize): p for p in predict_regions[0:30]}
+    futures = {executor.submit(predict_region, xgb, df_recurrent,
+                               label, dcols, p, lookbackSize): p for p in predict_regions}
 
     for future in concurrent.futures.as_completed(futures):
       pr = futures[future]
       try:
         data = future.result()
-        prediction.loc[pr[0]:pr[1]-1, label] = data
+        df.loc[pr[0]:pr[1]-1, label + '_pred' ] = data
       except Exception as exc:
         print(f'Generated an exception: {exc}')
       # else:
@@ -149,114 +139,131 @@ def repair_regions(df, label, max_size=None, lookbackSize=None, extra_features=N
     start, stop = p[0] - 100, p[1] + 100
     fig = go.Figure()
     fig.add_trace(go.Scatter(
-        x=df_att[label][start:stop].index,
-        y=df_att[label][start:stop],
+        x=df[label][start:stop].index,
+        y=df[label][start:stop],
         mode='lines', name='Real', connectgaps=False))
     fig.add_trace(go.Scatter(
-        x=prediction[label][start:stop]
-        .where(prediction[label + '_error'][start:stop]).index,
-        y=prediction[label][start:stop]
-        .where(prediction[label + '_error'][start:stop]),
+        x=df[label + '_pred'][start:stop]
+        .where(df[label + '_error'][start:stop]).index,
+        y=df[label + '_pred'][start:stop]
+        .where(df[label + '_error'][start:stop]),
         mode='lines', name='Prediction'))
 
     fig.write_image(f'images/{label}_{i}.jpg')
 
+  #df.loc[:,label] = prediction[label]
 
 if __name__ == '__main__':
   root = os.getcwd()
   data_path = pjoin(
       root, 'data/cleandata/Info pluviometricas/Merged Data/merged_wRegions.csv')
 
-  # save_path = pjoin(
-  #     root, '')
+  save_path = pjoin(root, 'data/cleandata/Info pluviometricas/Merged Data/merged_Repaired.csv')
 
   df = pd.read_csv(data_path,
                    sep=';',
                    dtype={'Local_0': object, 'Local_1': object,
                           'Local_2': object, 'Local_3': object})
 
+  df[['Date', 'Time']] = df['Data / Hora'].str.split(expand=True)
+  df[['Hora', 'Min', 'Seg']] = df['Time'].str.split(':', expand=True)
+  df['Hora'] = df['Hora'].astype(int)
+  df['Min'] = df['Min'].astype(int)
+  df['Seg'] = df['Seg'].astype(int)
+
   config = {
       'UmidadeRelativa':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'PressaoAtmosferica':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'Temperatura do Ar':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'TemperaturaInterna':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'PontoDeOrvalho':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'SensacaoTermica':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'RadiacaoSolar':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'DirecaoDoVento':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'VelocidadeDoVento':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
 
       'Precipitacao':
-      {'max_size': 5,
-       'lookbackSize': 30,
+      {'max_interpolation_size': 5,
+       'max_region_size':100,
+       'lookbackSize': 5,
        'extra_features': ['Hora']},
   }
 
-r = re.compile(".*error")
-cols = list(filter(r.match, df.columns.to_list()))
-cols = [i for i in cols if 'Precipitacao' not in i]
+  r = re.compile(".*error")
+  cols = list(filter(r.match, df.columns.to_list()))
+  cols = [i for i in cols if 'Precipitacao' not in i]
 
-df_cols = pd.DataFrame(columns=['Coluna', 'Valor'])
+  df_cols = pd.DataFrame(columns=['Coluna', 'Valor'])
 
-for col in cols:
-    #print(col)
-    #display(df[col].value_counts())
-    val = round(df[col].value_counts()[True] / df.shape[0] * 100, 2)
-    df_cols.loc[df_cols.shape[0]] = [col, val]
+  for col in cols:
+      val = round(df[col].value_counts()[True] / df.shape[0] * 100, 2)
+      df_cols.loc[df_cols.shape[0]] = [col, val]
 
-df_cols = df_cols.sort_values(by='Valor').reset_index(drop=True)
-df_cols.iloc[:, 0] = df_cols.iloc[:, 0].str.replace("_error", "")
+  df_cols = df_cols.sort_values(by='Valor').reset_index(drop=True)
+  df_cols.iloc[:, 0] = df_cols.iloc[:, 0].str.replace("_error", "")
 
-logging.info('## Interpolating Data')
-for i in range(len(df_cols)):
-  label = df_cols.iloc[i, 0]
+  logging.info('## Interpolating Data')
+  for i in range(len(df_cols)):
+    label = df_cols.iloc[i, 0]
 
-  error = df[df_cols.iloc[i, 0] + '_error'].copy(deep=True)
-  Series = df[df_cols.iloc[i, 0]].copy(deep=True)
+    error = df[df_cols.iloc[i, 0] + '_error'].copy(deep=True)
+    Series = df[df_cols.iloc[i, 0]].copy(deep=True)
 
-  n_Series, n_error = interpolation(
-      Series, error, **config[label.split('_')[0]])
-  df.loc[:, df_cols.iloc[i, 0] + '_error'] = n_error
-  df.loc[:, df_cols.iloc[i, 0]] = n_Series
+    n_Series, n_error, error_interpol = interpolation(
+                                        Series, error, **config[label.split('_')[0]])
+    df.loc[:, df_cols.iloc[i, 0] + '_error'] = n_error
+    df.loc[:, df_cols.iloc[i, 0] + '_interpol'] = error_interpol
+    df.loc[:, df_cols.iloc[i, 0]] = n_Series
 
-logging.info('XGB')
-for i in range(len(df_cols)):
-  label = df_cols.iloc[i, 0]
-  logging.info(f'({i}/{len(df_cols)}) Training for {label}')
-  repair_regions(df, label, **config[label.split('_')[0]])
-  logging.info(f'({i}/{len(df_cols)}) Done training for {label}')
+  logging.info(' ## XGB Regression')
+  for i in range(len(df_cols)):
+    label = df_cols.iloc[i, 0]
+    logging.info(f'({i}/{len(df_cols)}) Training for {label}')
+    repair_regions(df, label, **config[label.split('_')[0]])
+    logging.info(f'({i}/{len(df_cols)}) Done training for {label}\n')
+
+  df.to_csv(save_path)
