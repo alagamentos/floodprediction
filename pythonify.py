5e6cfbd130d0c0c66dbe0f041554c04a7e4c6b5f
Felipe Andrade <fasa.work@gmail.com>
2020-08-18 11:55:57 -0300

    Implement line enumeration removal for pythonify.py

--- pythonify.py
+++ pythonify.py
@@ -1,22 +1,37 @@
 import os
+import re
 from subprocess import PIPE, run
 
+# Get paths from all staged files
 command = 'git diff --cached --name-only'
-
-files = run(command.split(' '), stdout=PIPE) \
-          .stdout.decode('unicode_escape') \
-          .encode('latin1').decode('utf-8') \
-          .replace('"', '').splitlines()
+files = run(command.split(' '), stdout=PIPE).stdout.decode('unicode_escape') \
+          .encode('latin1').decode('utf-8').replace('"', '').splitlines()
 
 for file in files:
   # Check whether file was deleted or it's not a Jupyter Notebook
   if not os.path.exists(file) or '.ipynb' not in file:
     continue
 
-  dirname = f'{os.path.dirname(file)}/py'
+  py_dirname = f'{os.path.dirname(file)}/py'
+  py_filename = os.path.basename(file).replace('.ipynb', '.py')
 
-  if not os.path.exists(dirname):
-    os.mkdir(dirname)
+  if not os.path.exists(py_dirname):
+    os.mkdir(py_dirname)
 
-  strReturn = f'jupyter nbconvert "{file}" --to="python" --output-dir={dirname}'
-  os.system(strReturn)
+  # Convert Jupyter Notebooks to Python files
+  command = f'jupyter nbconvert "{file}" --to="python" --output-dir={py_dirname}'
+  os.system(command)
+
+  # Remove line enumeration to avoid undesired file changes
+  newFileData = ''
+
+  with open(f'{py_dirname}/{py_filename}', 'r+') as filedata:
+    for line in filedata:
+      if re.match('# In\[(\d|\s)+\]:', line):
+        line = '# In[ ]:\n'
+
+      newFileData += line
+
+    filedata.seek(0)
+    filedata.truncate(0)
+    filedata.write(newFileData)
