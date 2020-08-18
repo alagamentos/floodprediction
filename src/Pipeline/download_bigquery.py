d066f3994774b7202d39d5e187afcd3026e0a71d
Felipe Andrade <fasa.work@gmail.com>
2020-08-18 12:18:18 -0300

    Change variables name

--- src/Pipeline/download_bigquery.py
+++ src/Pipeline/download_bigquery.py
@@ -3,19 +3,22 @@ from google.oauth2 import service_account
 import pandas as pd
 import os
 
-# Windows
+# Workaround for Windows
 try:
   import google.cloud.bigquery_storage_v1.client
   from functools import partialmethod
+
   # Set a two hours timeout
   google.cloud.bigquery_storage_v1.client.BigQueryReadClient.read_rows = \
       partialmethod(google.cloud.bigquery_storage_v1.client.BigQueryReadClient.read_rows, timeout=3600*2)
 except:
   pass
 
+
 def create_path(path):
   if not os.path.exists(path):
     os.mkdir(path)
+
 
 create_path('data/cleandata/Info pluviometricas')
 create_path('data/cleandata/Info pluviometricas/Merged Data')
@@ -27,25 +30,35 @@ TABLE_merged = 'info_pluviometrica.merged'
 TABLE_regions = 'info_pluviometrica.error_regions'
 TABLE_repaired = 'info_pluviometrica.repaired'
 TABLE_lat_lng_estacoes = 'estacoes.lat_lng_estacoes'
-TABLE_openwehather_hisotry = 'openweather.history'
+TABLE_owm_history_bulk = 'openweather.history'
 
 CREDENTIALS = service_account.Credentials.from_service_account_file('key/temporal-285820-cde76c259484.json')
 
+
 df_merged = pd.read_gbq(f'SELECT * FROM {PROJECT_ID}.{TABLE_merged}', credentials=CREDENTIALS, project_id=PROJECT_ID)
 df_merged.to_csv('data/cleandata/Info pluviometricas/Merged Data/merged.csv',  decimal='.', sep=';', index=False)
+print('merged done!')
 
 
 df_regions = pd.read_gbq(f'SELECT * FROM {PROJECT_ID}.{TABLE_regions}', credentials=CREDENTIALS, project_id=PROJECT_ID)
-df_regions.to_csv('data/cleandata/Info pluviometricas/Merged Data/error_regions.csv',  decimal='.', sep=';', index=False)
+df_regions.to_csv('data/cleandata/Info pluviometricas/Merged Data/error_regions.csv',
+                  decimal='.', sep=';', index=False)
+print('error_regions done!')
 
 
-df_repaired = pd.read_gbq(f'SELECT * FROM {PROJECT_ID}.{TABLE_repaired}', credentials=CREDENTIALS, project_id=PROJECT_ID)
+df_repaired = pd.read_gbq(f'SELECT * FROM {PROJECT_ID}.{TABLE_repaired}',
+                          credentials=CREDENTIALS, project_id=PROJECT_ID)
 df_repaired.to_csv('data/cleandata/Info pluviometricas/Merged Data/repaired.csv',  decimal='.', sep=';', index=False)
+print('repaired done!')
 
 
-df_lng_lng_estacoes = pd.read_gbq(f'SELECT * FROM {PROJECT_ID}.{TABLE_lat_lng_estacoes}', credentials=CREDENTIALS, project_id=PROJECT_ID)
+df_lng_lng_estacoes = pd.read_gbq(
+    f'SELECT * FROM {PROJECT_ID}.{TABLE_lat_lng_estacoes}', credentials=CREDENTIALS, project_id=PROJECT_ID)
 df_lng_lng_estacoes.to_csv('data/cleandata/Estacoes/lat_lng_estacoes.csv',  decimal='.', sep=';', index=False)
+print('lat_lng_estacoes done!')
 
 
-df_openweather_history =  pd.read_gbq(f'SELECT * FROM {PROJECT_ID}.{TABLE_openwehather_hisotry}', credentials=CREDENTIALS, project_id=PROJECT_ID)
+df_openweather_history = pd.read_gbq(
+    f'SELECT * FROM {PROJECT_ID}.{TABLE_owm_history_bulk}', credentials=CREDENTIALS, project_id=PROJECT_ID)
 df_openweather_history.to_csv('data/cleandata/openweather/history_bulk.csv',  decimal='.', sep=';', index=False)
+print('openweathermap history bulk done!')
--- src/Pipeline/upload_bigquery.py
+++ src/Pipeline/upload_bigquery.py
@@ -4,49 +4,44 @@ import pandas as pd
 
 PROJECT_ID = 'temporal-285820'
 TABLE_merged = 'info_pluviometrica.merged'
-TABLE_merged_wregions = 'info_pluviometrica.regions'
+TABLE_error_regions = 'info_pluviometrica.regions'
 TABLE_repaired = 'info_pluviometrica.repaired'
 TABLE_lat_lng_estacoes = 'estacoes.lat_lng_estacoes'
 TABLE_openwehather_hisotry = 'openweather.history'
 
-CREDENTIALS   = service_account.Credentials.from_service_account_file('key/temporal-285820-cde76c259484.json')
+CREDENTIALS = service_account.Credentials.from_service_account_file('key/temporal-285820-cde76c259484.json')
 pandas_gbq.context.credentials = CREDENTIALS
 
-# merged.csv
-df_merged = pd.read_csv('data/cleandata/Info pluviometricas/Merged Data/merged.csv',
-                        sep=';',
-                        dtype={'Local_0': object, 'Local_1': object,
-                               'Local_2': object, 'Local_3': object})
+
+df_merged = pd.read_csv('data/cleandata/Info pluviometricas/Merged Data/merged.csv', sep=';',
+                        dtype={'Local_0': object, 'Local_1': object, 'Local_2': object, 'Local_3': object})
 
 pandas_gbq.to_gbq(df_merged, TABLE_merged, project_id=PROJECT_ID, credentials=CREDENTIALS, if_exists='replace')
-print('df_merged done!')
+print('merged done!')
 
 
-# merged_wRegions.csv
-df_regions = pd.read_csv('data/cleandata/Info pluviometricas/Merged Data/error_regions.csv',
-                                 sep=';')
+df_regions = pd.read_csv('data/cleandata/Info pluviometricas/Merged Data/error_regions.csv', sep=';')
 
-pandas_gbq.to_gbq(df_regions, TABLE_merged_wregions, project_id=PROJECT_ID, credentials=CREDENTIALS, if_exists='replace')
-print('merged_wRegions done!')
+pandas_gbq.to_gbq(df_regions, TABLE_error_regions, project_id=PROJECT_ID,
+                  credentials=CREDENTIALS, if_exists='replace')
+print('error_regions done!')
 
 
-# merged_Repaired.csv
-df_repaired = pd.read_csv('data/cleandata/Info pluviometricas/Merged Data/repaired.csv',
-            sep = ';')
+df_repaired = pd.read_csv('data/cleandata/Info pluviometricas/Merged Data/repaired.csv', sep=';')
 
 pandas_gbq.to_gbq(df_repaired, TABLE_repaired, project_id=PROJECT_ID, credentials=CREDENTIALS, if_exists='replace')
-print('regions done!')
+print('repaired done!')
 
-# lat_lng_estacoes
-df_lat_lng_estacoes = pd.read_csv( 'data/cleandata/Estacoes/lat_lng_estacoes.csv',
-            sep = ';')
 
-pandas_gbq.to_gbq(df_lat_lng_estacoes, TABLE_lat_lng_estacoes, project_id=PROJECT_ID, credentials=CREDENTIALS, if_exists='replace')
+df_lat_lng_estacoes = pd.read_csv('data/cleandata/Estacoes/lat_lng_estacoes.csv', sep=';')
+
+pandas_gbq.to_gbq(df_lat_lng_estacoes, TABLE_lat_lng_estacoes, project_id=PROJECT_ID,
+                  credentials=CREDENTIALS, if_exists='replace')
 print('lat_lng_estacoes done!')
 
-# history bulk openweather
-openweather_history = pd.read_csv('data/cleandata/openweather/history_bulk.csv',
-            sep = ';')
 
-pandas_gbq.to_gbq(openweather_history, TABLE_openwehather_hisotry, project_id=PROJECT_ID, credentials=CREDENTIALS, if_exists='replace')
-print('history bulk openweather done!')
+df_owm_history_bulk = pd.read_csv('data/cleandata/openweather/history_bulk.csv', sep=';')
+
+pandas_gbq.to_gbq(df_owm_history_bulk, TABLE_openwehather_hisotry,
+                  project_id=PROJECT_ID, credentials=CREDENTIALS, if_exists='replace')
+print('openweathermap history bulk done!')
