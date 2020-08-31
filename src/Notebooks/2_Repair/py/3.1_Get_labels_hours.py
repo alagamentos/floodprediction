6312eec6d2926e8d55ba5b2b8bfc84ad3ca6ec23
Felipe Ippolito <felipeippolito@gmail.com>
2020-08-31 05:20:24 -0300

    get label by the hour

--- src/Notebooks/2_Repair/py/3.1_Get_labels_hours.py
+++ src/Notebooks/2_Repair/py/3.1_Get_labels_hours.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ### Achar Labels
+# ### Achar Labels - Hora
 # 
 # Tentar achar a período de enchente verdadeiro baseado nas ordens de serviço e nos valores pluviométricos
 
@@ -11,149 +11,97 @@
 import pandas as pd
 import plotly as py
 from plotly import graph_objects as go
+from plotly.subplots import make_subplots
+import datetime
 
 py.offline.init_notebook_mode()
 
-ow = pd.read_csv('../../../data/cleandata/OpenWeather/history_bulk.csv', sep = ';',
-                 parse_dates = ['Data_Hora'])
+ow = pd.read_csv('../../../data/cleandata/OpenWeather/history_bulk.csv', sep = ';'
+                )
 
 ip = pd.read_csv('../../../data/cleandata/Info pluviometricas/Merged Data/merged.csv',
                  sep = ';',
                  dtype = {'Local_0': object, 'Local_1':object,
-                          'Local_2': object, 'Local_3':object},
-                 parse_dates = ['Data_Hora'])
+                          'Local_2': object, 'Local_3':object}
+                )
 
-ords = pd.read_csv('../../../data/cleandata/Ordens de serviço/Enchentes_LatLong.csv',
+ords = pd.read_csv('../../../data/cleandata/Ordens de serviço/labels_day.csv',
                  sep = ';')
 
 
-# #### Agrupar Ordens de Serviço por hora - Count( )
-# Total de ordens de serviço por hora
+# #### Ordens de Serviço
 
 # In[ ]:
 
 
-ords['Data'] = pd.to_datetime(ords['Data']).dt.date
-ords['Hora'] = pd.to_datetime(ords['Hora'], format='%H:%M:%S').dt.hour
+ords['Data'] = pd.to_datetime(ords['Data'], yearfirst = True)
 
 
-# In[ ]:
-
-
-ords_gb = ords.fillna(0).groupby(by=['Data', 'Hora']).count().max(axis=1).to_frame()
-ords_gb.columns = ['OrdensServico']
-ords_gb.index
-
-
-# 
-# Vizualizar os dados de pluviometria junto com os dados de ordens de serviço
-
-# #### Agrupar dados de precipitação por hora 
-# Reamostrando os dados de hora em hora
+# #### Info Pluviométrica
 
 # In[ ]:
 
 
+ip['Data_Hora'] = pd.to_datetime(ip['Data_Hora'], yearfirst=True)
 ip.insert(0, 'Data', ip.loc[:,'Data_Hora'].dt.date)
 ip.insert(0, 'Hora', ip.loc[:,'Data_Hora'].dt.hour)
-
-
-# In[ ]:
-
 
 precipitacao_cols = [c for c in ip.columns if 'Precipitacao'in c]
 precipitacao_cols += ['Data', 'Hora']
 
 df_p = ip[ip.loc[:,'Data_Hora'].dt.minute == 0][precipitacao_cols].reset_index(drop=True)
-df_p = df_p.fillna(0).groupby(by=['Data', 'Hora']).sum()
-df_p.index
+df_p['Data'] = pd.to_datetime(df_p['Data'], yearfirst = True)
+
+
+# #### OpenWeather
+
+# In[ ]:
+
+
+ow['Data_Hora'] = pd.to_datetime(ow['Data_Hora'], yearfirst=True)
+ow.insert(0, 'Data', ow.loc[:,'Data_Hora'].dt.date)
+ow.insert(0, 'Hora', ow.loc[:,'Data_Hora'].dt.hour)
+ow = ow[['Data','Hora','Precipitacao']]
+ow['Data'] = pd.to_datetime(ow['Data'], yearfirst = True)
+
+
+# #### Merge
+
+# In[ ]:
+
+
+# Merge Infopluviometrica with OpenWeather
+df_m = df_p.merge(ow, how = 'outer', on = ['Data','Hora']).sort_values(by = ['Data', 'Hora'])
+display(df_m.head(2))
+display(df_m.tail(2))
 
 
 # In[ ]:
 
 
-df_m = df_p.merge(ords_gb, how='outer', left_index=True, right_index=True)
-df_m['OrdensServico'] = df_m['OrdensServico'].fillna(0)
-df_m
+# Merge with OrdensServico
+df_m = df_m.merge(ords, on = 'Data', how = 'outer')
+df_m = df_m.fillna(0)
+df_m = df_m.rename(columns = {'Precipitacao':'Precipitacao_5'})
+df_m.insert(0,'Data_Hora', 0 )
+df_m['Data_Hora'] = pd.to_datetime(df_m['Data'].astype(str) + ' ' +
+                                   df_m['Hora'].astype(str) + ':00:00', yearfirst=True)
+df_m = df_m.rename(columns = {'LocalMax_ow':'LocalMax_5'})
+display(df_m[df_m['LocalMax_4'] == 1].head(2))
 
 
 # In[ ]:
 
 
-df_aux = df_m.reset_index()
-df_aux[df_aux['Data'] == '2019-08-30']
+fig = make_subplots(3,1, shared_xaxes=True)
 
+precipitacao_cols = [c for c in df_m.columns if 'Precipitacao'in c]
 
-# In[ ]:
+ano = 2019
+mes = 2
 
-
-display(df_m[df_m['Precipitacao_0'].isna()].head(2))
-display(df_m[df_m['Precipitacao_0'].isna()].tail(2))
-
-
-# In[ ]:
-
-
-ow.insert(0,'Data', ow.loc[:,'Data_Hora'].dt.date)
-ow.insert(0,'Hora', ow.loc[:,'Data_Hora'].dt.hour)
-
-
-# In[ ]:
-
-
-ow.groupby(by=['Data', 'Hora']).sum()[['Precipitacao']]
-
-
-# In[ ]:
-
-
-ow_gb = ow.groupby(by=['Data', 'Hora']).sum()[['Precipitacao']]
-ow_gb.columns = ['Precipitacao_ow']
-ow_gb.index
-
-
-# In[ ]:
-
-
-df_m = ow_gb.merge(df_m, how='outer', left_index=True, right_index=True)
-df_m['OrdensServico'] = df_m['OrdensServico'].fillna(0)
-df_m
-
-
-# In[ ]:
-
-
-df_aux = df_m.reset_index()
-df_aux[df_aux['Data'] == '2019-08-30']
-
-
-# In[ ]:
-
-
-ow_gb
-
-
-# In[ ]:
-
-
-from plotly.subplots import make_subplots
-fig = make_subplots(2,1, shared_xaxes=True)
-
-precipitacao_cols = [c for c in ip.columns if 'Precipitacao'in c]
-
-ano = 2011
-
-ip_ano = df_p.reset_index()
-ip_ano['Data_Hora'] = pd.to_datetime(ip_ano['Data'].astype(str) + ' ' + ip_ano['Hora'].astype(str) + ':00:00', yearfirst=True)
-ip_ano = ip_ano[ip_ano['Data_Hora'].dt.year == ano]
-
-ow_ano = ow_gb.reset_index()
-ow_ano['Data_Hora'] = pd.to_datetime(ow_ano['Data'].astype(str) + ' ' + ow_ano['Hora'].astype(str) + ':00:00', yearfirst=True)
-ow_ano = ow_ano[ow_ano['Data_Hora'].dt.year == ano]
-
-ords_gb_ano = ords_gb.reset_index()
-ords_gb_ano['Data_Hora'] = pd.to_datetime(ords_gb_ano['Data'].astype(str) + ' ' + ords_gb_ano['Hora'].astype(str) + ':00:00', yearfirst=True)
-ords_gb_ano = ords_gb_ano[ords_gb_ano['Data_Hora'].dt.year == ano]
+ip_ano = df_m[(df_m['Data'].dt.year == ano) & (df_m['Data'].dt.month == mes)]
+ords_ano = ords[(ords['Data'].dt.year == ano) & (ords['Data'].dt.month == mes)]
 
 for col in precipitacao_cols:
     fig.add_trace(
@@ -164,20 +112,20 @@ for col in precipitacao_cols:
             connectgaps=False),
         row = 1, col = 1)
 
-fig.add_trace(
-    go.Scatter(
-        x = ow_ano['Data_Hora'],
-        y = ow_ano['Precipitacao_ow'].fillna(0),
-        name = 'OpenWeather',
-        connectgaps=False),
-    row = 1, col = 1)
+for i in range(6):
+    fig.add_trace(
+        go.Bar(
+            x = ip_ano['Data_Hora'],
+            y = ip_ano[f'LocalMax_{i}'],
+            name = f'LocalMax_{i}',),
+        row = 2, col = 1)
 
 fig.add_trace(
     go.Bar(
-        x = ords_gb_ano['Data_Hora'],
-        y = ords_gb_ano['OrdensServico'],
-        name = 'Ordens de Serviço',),
-    row = 2, col = 1)
+        x = ords_ano['Data'] + datetime.timedelta(hours = 12),
+        y = ords_ano['LocalMax'],
+        name = 'Local Max',),
+    row = 3, col = 1)
 
 fig.show()
 
@@ -185,215 +133,64 @@ fig.show()
 # In[ ]:
 
 
-import sys
-sys.path.insert(1, '../../Pipeline')
-
-import imp
-import utils
-imp.reload(utils)
-from utils import *
-
-local_max = pd.read_csv('../../../data/cleandata/Info pluviometricas/Merged Data/merged_labeled.csv',
-                 sep = ';')
-
-
-# In[ ]:
-
-
-local_max['Data'] = pd.to_datetime(local_max['Data'], yearfirst=True)
-local_max.rename(columns={'Precipitacao_ow': 'Precipitacao_5'}, inplace=True)
-df_m = df_m.reset_index()
-df_m.rename(columns={'Precipitacao_ow': 'Precipitacao_5'}, inplace=True)
-
-display(local_max.head())
-display(df_m.head())
-
-
-# In[ ]:
-
-
-local_max_cols = [f'LocalMax_{i}' for i in range(6)]
-
-for col in local_max_cols:
-    local_max[col] = local_max['LocalMax']
-
-
-# In[ ]:
-
-
-rain_threshold = 10
+rain_threshold = 2
 
 for i in range(6):
-    local_max.loc[local_max[f'Precipitacao_{i}'] < rain_threshold, f'LocalMax_{i}'] = 0
-
-
-# In[ ]:
-
-
-local_max.head()
-
-
-# In[ ]:
-
-
-display(local_max[local_max['Data'] == '2019-08-30'])
-display(df_m[df_m['Data'] == '2019-08-30'])
-
-
-# #### Total de dias de ordem de serviço
-
-# In[ ]:
-
-
-(local_max['OrdensServico'].fillna(0) > 0).sum()
-
-
-# In[ ]:
-
-
-precipitacao_cols = [c for c in df_m.columns if 'Precipitacao' in c]
-precipitacao_cols.sort()
-cols = ['Data', 'Hora'] + precipitacao_cols
-
-df_mh = df_m.reset_index(drop=True)[cols]
-df_mh.head()
-
-
-# In[ ]:
-
-
-cols = ['Data', 'LocalMax'] + local_max_cols
-
-df_mh = df_mh.merge(local_max[cols], how='outer', on='Data')
-df_mh.head()
-
-
-# In[ ]:
-
-
-rain_threshold = 10
-
-for i in range(6):
-    df_mh.loc[df_mh[f'Precipitacao_{i}'] < rain_threshold, f'LocalMax_{i}'] = 0
-
-
-# In[ ]:
-
-
-df_mh.head()
-
-
-# In[ ]:
-
-
-df_mh[df_mh['LocalMax'] == 1)]
-
-
-# In[ ]:
-
-
-## LocalMax
-
-regions = list_2_regions(df_m['OrdensServico'].fillna(0) > 0)
-b_list = regions_2_list(regions, len(df_m))
-regions = list_2_regions(b_list)
-
-df_m.loc[:, 'LocalMax'] = 0
-
-df_m['OrdensServico'] = df_m['OrdensServico'].fillna(0)
-for r in regions:
-    for i in range(r[0], r[1]+1):
-        id_max = df_m.loc[i-3: i+3, 'OrdensServico'].idxmax()
-        if i == id_max:
-            df_m.loc[i-3: i+3, 'LocalMax'] = 0 
-            df_m.loc[i, 'LocalMax'] = 1
-
-
-# ### Label
-# Considera os 2 primeiros dias de ordens de serviço consecutivos. Desses 2 dias são comparadas as precipitações totais e o dia com maior precipitação é o Label.
-
-# In[ ]:
-
-
-## Label
-
-regions = list_2_regions(df_m['OrdensServico'].fillna(0) > 0)
-b_list = regions_2_list(regions, len(df_m))
-regions = list_2_regions(b_list)
-regions = [[r[0] - 1 , r[0] + 1] for r in regions]
-b_list = regions_2_list(regions, len(df_m))
-df_m.loc[:, 'Label'] = 0
-df_m.loc[b_list,'Label'] = 1
-
-
-# In[ ]:
-
-
-for r in regions:
-    if df_m.loc[r[0],precipitacao_cols].sum() >= df_m.loc[r[0] + 1,precipitacao_cols].sum():
-        df_m.loc[r[0] + 1, 'Label'] = 0 
-    else:
-        df_m.loc[r[0], 'Label' ] = 0
+    df_m.loc[df_m[f'Precipitacao_{i}'].fillna(0) < rain_threshold, f'LocalMax_{i}'] = 0
+    
+lm_cols = [c for c in df_m.columns if 'LocalMax_' in c]
+n_remove = len(df_m.loc[(df_m[lm_cols].max(axis = 1) == 0) &
+                      (df_m['LocalMax'] == 1), 'LocalMax'])
+print(f'Removing {n_remove} OrdensServico from LocalMax')
+df_m.loc[(df_m[lm_cols].max(axis = 1) == 0) &
+                      (df_m['LocalMax'] == 1), 'LocalMax'] = 0
 
 
 # In[ ]:
 
 
 from plotly.subplots import make_subplots
-fig = make_subplots(4,1, shared_xaxes=True )
+fig = make_subplots(3,1, shared_xaxes=True)
 
 precipitacao_cols = [c for c in ip.columns if 'Precipitacao'in c]
 
-ano = 2013
+ano = 2019
+mes = 2
 
-df_ano = df_m[df_m['Data'].dt.year == ano]
+ip_ano   = df_m[(df_m['Data'].dt.year == ano) & (df_m['Data'].dt.month == mes)]
+ords_ano = ords[(ords['Data'].dt.year == ano) & (ords['Data'].dt.month == mes)]
 
-for col in precipitacao_cols:
+for i in range(6):
+    fig.add_trace(
+        go.Scatter(
+            x = ip_ano['Data_Hora'],
+            y = ip_ano[f'Precipitacao_{i}'],
+            name = f'Precipitacao_{i}',
+            connectgaps=False),
+        row = 1, col = 1)
+
+for i in range(6):
     fig.add_trace(
         go.Bar(
-            x = df_ano['Data'],
-            y = df_ano[col],
-            name = col,),
-    row = 1, col = 1)
+            x = ip_ano['Data_Hora'],
+            y = ip_ano[f'LocalMax_{i}'],
+            name = f'LocalMax_{i}',),
+        row = 2, col = 1)
 
 fig.add_trace(
     go.Bar(
-        x = df_ano['Data'],
-        y = df_ano['OrdensServico'],
-        name = 'Ordens de Serviço',),
-    row = 2, col = 1)
-
-fig.add_trace(
-    go.Bar(
-        x = df_ano['Data'],
-        y = df_ano['Label'],
-        name = 'Label',),
+        x = ords_ano['Data'] + datetime.timedelta(hours = 12),
+        y = ords_ano['LocalMax'] ,
+        name = 'Local Max (Dia)',),
     row = 3, col = 1)
 
-fig.add_trace(
-    go.Bar(
-        x = df_ano['Data'],
-        y = df_ano['LocalMax'],
-        name = 'LocalMax',),
-    row = 4, col = 1)
-
-#fig.show()
+fig.show()
 
 
 # In[ ]:
 
 
-df_m.groupby('LocalMax').mean()
-
-
-# In[ ]:
-
-
-df_m.groupby('Label').mean()
-
-
-# In[ ]:
-
-
-#df_m.to_csv('../../../data/cleandata/Info pluviometricas/Merged Data/merged_labeled.csv', sep = ';', index=False)
+interest_cols = [c for c in df_m.columns if 'Local' in c]
+df_m = df_m[['Data_Hora']  + interest_cols]
+df_m.head()
 
---
+++ src/Pipeline/get_labels_hour.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='## get_labels_hour - %(levelname)s: %(message)s ')
+
+if __name__== '__main__':
+
+  rain_threshold = 2 # mm/h
+
+  open_weather_path = 'data/cleandata/OpenWeather/history_bulk.csv'
+  ip_path = 'data/cleandata/Info pluviometricas/Merged Data/merged.csv'
+  ords_path = 'data/cleandata/Ordens de serviço/labels_day.csv'
+
+  save_path = 'data/cleandata/Ordens de serviço/labels_hour.csv'
+
+  ow = pd.read_csv(open_weather_path, sep = ';')
+
+  ip = pd.read_csv(ip_path,
+                  sep = ';',
+                  dtype = {'Local_0': object, 'Local_1':object,
+                            'Local_2':object,  'Local_3':object})
+
+  ords = pd.read_csv(ords_path,
+                     sep = ';')
+
+
+  ## TO DO:
+  ## Merge (merged.csv) With repaired data (repaired.csv)
+  ## - Use correct data for removing OrdensServico
+  ## =======================
+
+  # OrdensServico
+  ords['Data'] = pd.to_datetime(ords['Data'], yearfirst = True)
+
+
+  # InfoPluviometrica
+  ip['Data_Hora'] = pd.to_datetime(ip['Data_Hora'], yearfirst=True)
+  ip.insert(0, 'Data', ip.loc[:,'Data_Hora'].dt.date)
+  ip.insert(0, 'Hora', ip.loc[:,'Data_Hora'].dt.hour)
+
+  precipitacao_cols = [c for c in ip.columns if 'Precipitacao'in c]
+  precipitacao_cols += ['Data', 'Hora']
+
+  df_p = ip[ip.loc[:,'Data_Hora'].dt.minute == 0][precipitacao_cols].reset_index(drop=True)
+  df_p['Data'] = pd.to_datetime(df_p['Data'], yearfirst = True)
+
+
+  # OpenWeather
+  ow['Data_Hora'] = pd.to_datetime(ow['Data_Hora'], yearfirst=True)
+  ow.insert(0, 'Data', ow.loc[:,'Data_Hora'].dt.date)
+  ow.insert(0, 'Hora', ow.loc[:,'Data_Hora'].dt.hour)
+  ow = ow[['Data','Hora','Precipitacao']]
+  ow['Data'] = pd.to_datetime(ow['Data'], yearfirst = True)
+
+
+  # Merge
+
+  # Merge Infopluviometrica with OpenWeather
+  df_m = df_p.merge(ow, how = 'outer', on = ['Data','Hora']).sort_values(by = ['Data', 'Hora'])
+
+  # Merge with OrdensServico
+  df_m = df_m.merge(ords, on = 'Data', how = 'outer')
+
+  # Clean Merged
+  df_m = df_m.fillna(0)
+  df_m = df_m.rename(columns = {'Precipitacao':'Precipitacao_5'})
+  df_m.insert(0,'Data_Hora', 0 )
+  df_m['Data_Hora'] = pd.to_datetime(df_m['Data'].astype(str) + ' ' +
+                                    df_m['Hora'].astype(str) + ':00:00', yearfirst=True)
+  df_m = df_m.rename(columns = {'LocalMax_ow':'LocalMax_5'})
+
+  # Remove OrdemServico when rain is under threshold
+  for i in range(6):
+    n_remove = len(df_m.loc[(df_m[f'Precipitacao_{i}'].fillna(0) < rain_threshold) &
+                            (df_m[f'LocalMax_{i}'] == 1), f'LocalMax_{i}'])
+    logging.info(f'Removing {n_remove} OrdensServico from LocalMax')
+    df_m.loc[df_m[f'Precipitacao_{i}'].fillna(0) < rain_threshold, f'LocalMax_{i}'] = 0
+
+
+  lm_cols = [c for c in df_m.columns if 'LocalMax_' in c]
+  n_remove = len(df_m.loc[(df_m[lm_cols].max(axis = 1) == 0) &
+                          (df_m['LocalMax'] == 1), 'LocalMax'])
+  logging.info(f'Removing {n_remove} OrdensServico from LocalMax')
+  df_m.loc[(df_m[lm_cols].max(axis = 1) == 0) &
+                          (df_m['LocalMax'] == 1), 'LocalMax'] = 0
+
+  # Clean up output
+  interest_cols = [c for c in df_m.columns if 'Local' in c]
+  df_m = df_m[['Data_Hora']  + interest_cols]
+  df_m.head()
+
+  # Export CSV
+  saving_info = f'Saving labels_day.csv to path:\n\t\t | - {save_path}'
+  logging.info(saving_info)
+  df_m.to_csv(save_path, sep = ';', index = False)
